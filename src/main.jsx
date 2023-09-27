4f0fe144cd63512eaff424904042fca372ebe8ab
HoangPhucss <phuchihi321@gmail.com>
2023-09-28 06:55:14 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './weather/App'
+import 'antd/dist/reset.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>,
+)
